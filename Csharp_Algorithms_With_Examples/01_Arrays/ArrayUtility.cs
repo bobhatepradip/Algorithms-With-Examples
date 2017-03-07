0f0282f1fd5fded608297f42234e47fe7b570d7d
Pradip Bobhate <pradip.bobhate@live.in>
2017-03-07 01:57:08 -0800

    Array Peaks and Valleys

--- Csharp_Algorithms_With_Examples/01_Arrays/ArrayUtility.cs
+++ Csharp_Algorithms_With_Examples/01_Arrays/ArrayUtility.cs
@@ -6,13 +6,19 @@ namespace TWL_Algorithms_Samples.Arrays
     {
         public void Run()
         {
+            new Peaks_and_Valleys().Run();
+            //this.Merge_Run();
+        }
+
+        public void Merge_Run()
+        {
             int[] a = new int[] { 2, 3, 4, 5, 6, 8, 10, 100, 0, 0, 0, 0, 0, 0 };
             int[] b = new int[] { 1, 4, 5, 6, 7, 7 };
             int lastA = 8;
             int lastB = 6;
 
             Merge(a, b, lastA, lastB);
-            a.Print("Meaged Array a:");
+            a.Print("Merged Array a:");
         }
 
         /// <summary>
@@ -49,6 +55,97 @@ namespace TWL_Algorithms_Samples.Arrays
                 //a.Print();
                 //Console.ReadLine();
             }
+        }
+
+        public class Peaks_and_Valleys : IQuestion
+        {
+            
+            public void SortValleyPeak_UsingSorting(int[] array)
+            {
+                Array.Sort(array);
+                for (int i = 1; i < array.Length; i += 2)
+                {
+                    array.Swap(i - 1, i);
+                }
+            }
+
+           
+            public void SortValleyPeak_UsingMaxIndex(int[] array)
+            {
+                for (int i = 1; i < array.Length; i += 2)
+                {
+                    int biggestIndex = SortValleyPeakMaxIndex(array, i - 1, i, i + 1);
+                    if (i != biggestIndex)
+                    {
+                        array.Swap(i, biggestIndex);
+                    }
+                    array.Print();
+                }
+            }
+
+            public int SortValleyPeakMaxIndex(int[] array, int a, int b, int c)
+            {
+                int len = array.Length;
+                int aValue = a >= 0 && a < len ? array[a] : int.MinValue;
+                int bValue = b >= 0 && b < len ? array[b] : int.MinValue;
+                int cValue = c >= 0 && c < len ? array[c] : int.MinValue;
+
+                int max = Math.Max(aValue, Math.Max(bValue, cValue));
+
+                if (aValue == max)
+                {
+                    return a;
+                }
+                else if (bValue == max)
+                {
+                    return b;
+                }
+                else
+                {
+                    return c;
+                }
+            }
+
+            
+            public static void SortValleyPeak3(int[] array)
+            {
+                for (int i = 1; i < array.Length; i += 2)
+                {
+                    if (array[i - 1] < array[i])
+                    {
+                        array.Swap(i - 1, i);
+                        //array.Print();
+                    }
+                    if (i + 1 < array.Length && array[i + 1] < array[i])
+                    {
+                        array.Swap(i + 1, i);
+                        //array.Print();
+                    }
+                }
+            }
+
+            public void Run()
+            {
+                int[] array = { 48, 40, 31, 62, 28, 21, 64, 40, 23, 17 };
+                //int[] array2 = array;
+                //int[] array3 = array;
+                array.Print();
+                SortValleyPeak_UsingSorting(array);
+                array.Print();
+                Console.WriteLine("=================================================");
+                int[] array2 = { 48, 40, 31, 62, 28, 21, 64, 40, 23, 17 };
+                array2.Print();
+                SortValleyPeak_UsingMaxIndex(array2);
+                array2.Print();
+                Console.WriteLine("=================================================");
+                int[] array3 = { 48, 40, 31, 62, 28, 21, 64, 40, 23, 17 };
+                array3.Print();
+                SortValleyPeak3(array3);
+                array3.Print();
+                Console.WriteLine("=================================================");
+            }
+
+           
         }
     }
 }
